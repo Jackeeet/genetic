fdf41337f31de393246de2aac6343ad2344f507b
Polina Grabar <polina.grabar@gmail.com>
2022-11-04 16:44:38 +0400

    scores

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,19 @@
 {-# OPTIONS_GHC -Wno-type-defaults #-}
 module Main(main) where
 
-import           GeneticLib    (newPopulation)
+import           Data.List  (sort)
+import           GeneticLib (getFitness, newPopulation, nextGeneration)
 
 main :: IO ()
 main = do
-    pop <- newPopulation solutionRange populationSize
-    print pop
-   --  print $ randomPairs pop 2
+    let offset = if solutionFrom < 0 then (-1) * solutionFrom else 0
+    initial <- newPopulation (solutionFrom + offset, solutionTo + offset) populationSize
+    print initial
+    nextGen <- nextGeneration initial crossoverCount
+    print nextGen
+    let scores = getFitness ((-1) * offset) f nextGen
+    print scores
+
 
 f :: Int -> Int
 f x = a + b * x + c * (x ^ 2) + d * (x ^ 3)
@@ -16,16 +22,15 @@ f x = a + b * x + c * (x ^ 2) + d * (x ^ 3)
           c = -66
           d = 1
 
-solutionRange :: (Int, Int)
-solutionRange = (-10, 53)
+solutionFrom :: Int
+solutionFrom = -10
+
+solutionTo :: Int
+solutionTo = 53
 
 populationSize :: Int
 populationSize = 4
 
 crossoverCount :: Int
 crossoverCount = 2
-
-mutationCount :: Int
-mutationCount = 1
-
 
--- src/GeneticLib.hs
+++ src/GeneticLib.hs
@@ -1,32 +1,40 @@
-module GeneticLib (newPopulation, scores, crossover, mutate, generateAll) where
+module GeneticLib (newPopulation, nextGeneration, getFitness, select) where
 
 import           Data.Char     (digitToInt)
 import           Data.Functor  ((<&>))
 import           Data.List     (foldl')
-import           System.Random
 import           Support       (randomPairs)
+import           System.Random
 
 type Genotype = String
---type Gene = Char
 type FitFunc = (Int -> Int)
 
 geneLength :: Int
-geneLength = 7
+geneLength = 6
 
-scores :: FitFunc -> [Genotype] -> [Int]
-scores fitFunc = map $ fitFunc . toInt
+mutProbability :: Double
+mutProbability = 0.3
+
+getFitness :: Int -> FitFunc -> [Genotype] -> [Int]
+getFitness offset fitFunc = map $ fitFunc . (+offset) . toInt
 
 newPopulation :: (Int, Int) -> Int -> IO [Genotype]
 newPopulation range size = fmap (map toGenotype) (take size . randomRs range <$> newStdGen)
 
--- nextGeneration :: [Genotype] -> [Genotype]
+nextGeneration :: [Genotype] -> Int -> IO [Genotype]
 nextGeneration pop crossCount = do
     pairs <- randomPairs pop crossCount
-    let nextGen = undefined
-    return undefined
+    nextPop <- generateAll pairs
+    let npCount = length nextPop
+    mps <- take npCount . randomRs (0.0 :: Double, 1.0 :: Double) <$> newStdGen
+    points <- take npCount . randomRs (1, geneLength) <$> newStdGen
+    return $ pop ++ [mutateCond gt point (prob <= mutProbability) | (gt, prob, point) <- zip3 nextPop mps points]
 
 select :: [Genotype] -> [Genotype]
 select pop = undefined
+
+mutateCond :: Genotype -> Int -> Bool -> Genotype
+mutateCond gt point apply = if apply then mutate gt point else gt
 
 mutate :: Genotype -> Int -> Genotype
 mutate gt point = take (point - 1) gt ++ [invert $ gt !! (point - 1)] ++ drop point gt
@@ -34,25 +42,22 @@ mutate gt point = take (point - 1) gt ++ [invert $ gt !! (point - 1)] ++ drop po
           invert '1' = '0'
           invert _   = undefined
 
--- все родительские особи + все новые особи
 generateAll :: [(Genotype, Genotype)] -> IO [Genotype]
 generateAll pairs = mapM generate pairs <&> concat
 --generateAll pairs = mapM generate pairs >>= (return . concat)
 
--- 2 родительские особи + 2 новых особи
 generate :: (Genotype, Genotype) -> IO [Genotype]
 generate (g1, g2) = do
     crossPoint <- randomRIO (1, geneLength)
-    return [crossover g1 g2 crossPoint, crossover g2 g1 crossPoint, g1, g2]
+    return [crossover g1 g2 crossPoint, crossover g2 g1 crossPoint]
 
 crossover :: Genotype -> Genotype -> Int -> Genotype
 crossover first second point = take point first ++ drop point second
 
 toGenotype :: Int -> Genotype
 toGenotype 0 = "0"
-toGenotype n | n > 0 = '0' : binary n
-            | otherwise = '1' : binary (-1 * n)
-    where binary num = padZero (geneLength - 1) (reverse $ helper num)
+toGenotype n = binary n
+    where binary num = padZero geneLength $ reverse $ helper num
           padZero maxLength str = replicate (maxLength - length str) '0' ++ str
           helper 0 = ""
           helper n' | n' `mod` 2 == 1 = '1' : helper m
@@ -60,8 +65,4 @@ toGenotype n | n > 0 = '0' : binary n
                         where m = n' `div` 2
 
 toInt :: Genotype -> Int
-toInt [] = undefined -- to make the compiler stop complaining about non-exhaustive pattern matching
-toInt (g:gs) | g == '0' = binToInt gs
-             | g == '1' = (-1) * binToInt gs
-             | otherwise = undefined -- same here
-    where binToInt = foldl' (\acc x -> acc * 2 + digitToInt x) 0
+toInt = foldl' (\acc x -> acc * 2 + digitToInt x) 0
